2532337137d3190168bb2cde76eb4185a4ee704e
eliasjovancicpersson <eliasjovancicpersson@gmail.com>
2023-04-04 18:47:21 +0200

    settings fixed

--- react-app/src/components/Work.jsx
+++ react-app/src/components/Work.jsx
@@ -1,23 +1,35 @@
-import './styles/Work.css';
-import { Document, Page, pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-function Work() {
+import './styles/Work.css'
+import { Document, Page, pdfjs } from 'react-pdf'
+import { useState } from 'react'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+function Work () {
+  const projId = ''
+  const [data, setData] = useState(null)
+  setData(fetch('https://gyarb-backend.azurewebsites.net/wiki/' + projId))
+
   return (
-    <div className="work-container">
-      <div className="pdf-container">
-        <Document
-          file={'https://expresspdf.blob.core.windows.net/pdf/Bartenderbok-Fanny.pdf'}
-          className="pdf">
-          <Page pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} />
-        </Document>
-      </div>
-      <div className="page-controlls">
-        <input type="button" value="+" className="page-controll" />
-        <input type="number" name="page" id="page" />
-        <input type="button" value="-" className="page-controll" />
-      </div>
-    </div>
-  );
+    {
+      if (data) {
+        return (
+          <div className="work-container">
+            <div className="pdf-container">
+              <Document
+                file={data.pdf}
+                className="pdf">
+                <Page pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} />
+              </Document>
+            </div>
+            <div className="page-controlls">
+              <input type="button" value="+" className="page-controll" />
+              <input type="number" name="page" id="page" />
+              <input type="button" value="-" className="page-controll" />
+            </div>
+          </div>
+        )
+      }
+    }
+
+  )
 }
 
-export default Work;
+export default Work

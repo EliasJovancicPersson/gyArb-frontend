b0076ead91944a04652242316f6c15f50d48f832
Elias Jovancic Persson <EliasJovancicPersson@gmail.com>
2023-04-04 17:54:37 +0200

    page handler wip

--- react-app/src/components/Work.jsx
+++ react-app/src/components/Work.jsx
@@ -3,12 +3,20 @@ import { Document, Page, pdfjs } from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 function Work() {
   return (
-    <Document
-      file={
-        'https://expresspdf.blob.core.windows.net/pdf/Elias%20Jovancic%20Persson%20-%20Resume%CC%81.pdf'
-      }>
-      <Page pageNumber={1} />
-    </Document>
+    <div className="work-container">
+      <div className="pdf-container">
+        <Document
+          file={'https://expresspdf.blob.core.windows.net/pdf/Bartenderbok-Fanny.pdf'}
+          className="pdf">
+          <Page pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} />
+        </Document>
+      </div>
+      <div className="page-controlls">
+        <input type="button" value="+" className="page-controll" />
+        <input type="number" name="page" id="page" />
+        <input type="button" value="-" className="page-controll" />
+      </div>
+    </div>
   );
 }
 

03997734910a2150449a5c75c686814f62efcc81
eliasjovancicpersson <eliasjovancicpersson@gmail.com>
2023-04-05 18:54:46 +0200

    center search

--- react-app/src/components/Navigation.jsx
+++ react-app/src/components/Navigation.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import './styles/Navigation.css'
 import Search from '../images/Icons/Search.svg'
+import pfp from '../images/Icons/placeholdePfpPic.jpg'
 
 function Navigation (props) {
   return (
@@ -10,17 +11,19 @@ function Navigation (props) {
         <li className="logo">
           <h1>Gymnasiearbeten.se</h1>
         </li>
-        <li>
+        {props.isLoggedIn &&
+        <li className='search'>
           <div className="searchWrapper">
             <input type="search" placeholder="Sök på arbeten" />
-            <img src={Search} alt="Search icon" className="search" />
+            <img src={Search} alt="Search icon" className="search-img" />
           </div>
-        </li>
+        </li>}
         <li className="links">
           <div className="link-container">
             <Link to={'/'}>Hem</Link>
             {props.isLoggedIn && <Link to={'/work'}>Arbeten</Link>}
-            {props.isLoggedIn && <Link to={'/profile/' + props.user.id}>Profil</Link>}
+            {props.isLoggedIn && <Link to={'/work/upload'}>Ladda up</Link>}
+            {props.isLoggedIn && <Link className='profilePic' to={'/profile/' + props.user.id}><img src={pfp}></img></Link>}
             {!props.isLoggedIn && <Link to={'/login'}>Logga in</Link>}
           </div>
         </li>
